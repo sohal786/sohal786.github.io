f80f53cf950f8634b74c97d856e2fbb243c42480
dsav99 <savidsav99@gmail.com>
2024-03-14 08:04:20 -0700

    Final padding

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,12 +28,12 @@ const Hero = () => {
             <div className=' w-full md:max-w-[1780px] m-auto grid md:grid-cols-2 max-w-[600px]  px-4 md:px-14'>
 
                 <div className='flex flex-col justify-center gap-4'>
-                    <p className='sm:text-s md:text-xl py-0 md block font-medium' style={{ color: colors.white }}>Welcome to my Personal Website!</p>
-                    <h1 className='sm:text-3xl leading-[72px] md:text-6xl  font-semibold'>
+                    <p className='text-s py-0 md block font-medium' style={{ color: colors.white }}>Welcome to my Personal Website!</p>
+                    <h1 className='text-4xl leading-[72px] md:text-6xl  font-semibold'>
                         <span ref={el} style={{ color: colors.white }}></span>
                      </h1>
-                    <p className=' text-lg text-gray-600' style={{ color: colors.orange }}>Software Developer</p>
-                    <p className=' text-lg text-gray-600' style={{ color: colors.orange }}>San Francisco, CA</p>
+                    <p className=' text-base text-gray-600' style={{ color: colors.orange }}>Software Developer</p>
+                    <p className=' text-base text-gray-600' style={{ color: colors.orange }}>San Francisco, CA</p>
 
                     {/* <img src='ME.png'className='icon p-bottom-0 hidden md:flex' ></img> */}
                     {/* <p className='py-2 text-lg text-gray-600' style={{ color: colors.accent }}>Navigating the Future of Tech: A SF State Grad’s Quest for Innovation in Software Development.</p> */}
